dbb4c97cc88dec0767298656e1ccb8d11de07d71
Conor T. Ryan <ctryan3@gmail.com>
2021-02-19 09:42:26 -0800

    Tweaks to setup script

--- local-setup.sh
+++ local-setup.sh
@@ -3,23 +3,25 @@ VENV_FOLDER=.venv
 if [ ! -d "$VENV_FOLDER" ]; then
     echo "Creating virtual environment"
     python3 -m venv .venv
-    source .venv/bin/activate
 else
     echo "Virtual environment already installed"
 fi
+
+echo "Activating virtual environment"
+source .venv/bin/activate
 
 echo "Installing Python requirements"
 pip install -qr requirements.txt
 
 if [ ! -f "$VENV_FOLDER/bin/chromedriver" ]; then
-    echo "Installing Chrome WebDriver"
+    echo "Installing ChromeDriver"
     # platform options: linux32, linux64, mac64, win32
     PLATFORM=mac64
     VERSION=88.0.4324.96
     curl -s http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip | bsdtar -xvf - -C $VENV_FOLDER/bin
     chmod a+x $VENV_FOLDER/bin/chromedriver
 else   
-    echo "Chrome WebDriver already installed"
+    echo "ChromeDriver already installed"
 fi
 
 export CHROMEDRIVER_PATH="$(pwd)/$VENV_FOLDER/bin/chromedriver"
